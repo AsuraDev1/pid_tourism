b96c52c9c5c79aac33587dc3b3a9289470b5db65
Asura <jose254ernesto1@gmail.com>
2025-05-25 11:39:18 -0400

    deploy debug

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './',
+  base: 'https://asuradev1.github.io/pid_tourism/',
 })
